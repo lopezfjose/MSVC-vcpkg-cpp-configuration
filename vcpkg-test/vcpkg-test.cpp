aaa4d6eb48d3bcb4fb2d23a9695ec5c3fe5e4bb2
Jose <jflopezfernandez@gmail.com>
2018-06-02 12:00:48 -0400

    Initial commit. Project successfully builds and runs GTest and Catch for Win64 on Debug configuration.

---
+++ vcpkg-test/vcpkg-test.cpp
@@ -0,0 +1,34 @@
+﻿// vcpkg-test.cpp : Defines the entry point for the application.
+//
+
+#include "vcpkg-test.h"
+
+unsigned int factorial(const unsigned int number)
+{
+    return ((number <= 1) ? number : factorial(number - 1) * number);
+}
+
+/** Catch test framework sample test case
+TEST_CASE("Compute Factorials", "[factorial]")
+{
+    REQUIRE(factorial(1) == 1);
+    REQUIRE(factorial(2) == 2);
+    REQUIRE(factorial(3) == 6);
+}
+*/
+
+TEST(TestingConfiguration, BasicTest)
+{
+    EXPECT_EQ(0, 1);
+}
+
+
+int main(int argc, char *argv[])
+{
+    // Main function for Catch test framework
+    //return Catch::Session().run(argc, argv);
+     
+    testing::InitGoogleTest(&argc, argv);
+
+    return RUN_ALL_TESTS();
+}
---
+++ vcpkg-test/vcpkg-test.h
@@ -0,0 +1,15 @@
+﻿// vcpkg-test.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <cstdlib>
+
+#include <iostream>
+
+#include <boost/program_options.hpp>
+
+#define CATCH_CONFIG_RUNNER
+#include <Catch/catch.hpp>
+
+#include <gtest/gtest.h>
